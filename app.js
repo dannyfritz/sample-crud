e88b6ff3c53532672f226b0937b7d20ef1e58fcf
Danny Fritz <dannyfritz@gmail.com>
2015-12-14 11:21:09 -0700

    :sparkles: refactored express routes

--- app.js
+++ app.js
@@ -5,39 +5,38 @@ var bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/cities', function (req, res) {
-  api.cities.read()
-  .then(function (results) {
+function handleResponse(promise, res) {
+  if (!res) {
+    console.error('response object required for handleResponse')
+    return
+  }
+  promise.then(function (results) {
     res.json(results)
   })
+  .catch(function (err) {
+    res.statusCode = 503
+    res.json(err)
+  })
+}
+
+app.get('/cities', function (req, res) {
+  handleResponse(api.cities.read(), res)
 })
 
 app.get('/cities/:id', function (req, res) {
-  api.city.read(req.params.id)
-  .then(function (results) {
-    res.json(results)
-  })
+  handleResponse(api.city.read(req.params.id), res)
 })
 
 app.post('/cities', function (req, res) {
-  api.city.create(req.body.name)
-  .then(function (results) {
-    res.json(results)
-  })
+  handleResponse(api.city.create(req.body.name), res)
 })
 
 app.delete('/cities/:id', function (req, res) {
-  api.city.delete(req.params.id)
-  .then(function (results) {
-    res.json(results)
-  })
+  handleResponse(api.city.delete(req.params.id), res)
 })
 
 app.put('/cities/:id', function (req, res) {
-  api.city.update(req.params.id, req.body.name)
-  .then(function (results) {
-    res.json(results)
-  })
+  handleResponse(api.city.update(req.params.id, req.body.name), res)
 })
 
 app.listen(8080, function () {
